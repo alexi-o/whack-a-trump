9ba4047efbf2966ad4d6dfc1c49d1057946c1aac
AlexiOHearn <alexiohearn@o.local>
2017-03-27 17:15:38 -0600

    starts using canvas for project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log("JS WORKING");
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var stage = new Stage(canvas);
+
+stage.mouseEventsEnabled = true;
+
+trumpImg.src = "../images/trump1.png";
+
+var randomPos = Math.floor(Math.random() * 3);
+
+// Coordinates where a Trump can appear
+var trumpsX =[140, 200, 50];
+var trumpsY = [103, 150, 150];
+
+var currentTrumps = 0;
+var trumpWhacks = 0;
+var totalTrumps = 3;
+
+function Main()
+{
+
+}
+
+ctx.fillText('Spot 1', 140,103);
+ctx.fillText('Spot 2', 200,150);
+ctx.fillText('Spot 3', 50,150);
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+
